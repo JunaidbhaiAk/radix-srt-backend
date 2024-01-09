1622f0d7e15d3e16a06bcbad77cf35d118526a37
Junaid <junaidhonor8x@gmail.com>
2024-01-09 20:05:13 +0530

    added client origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'https://radix-srt.netlify.app'}));
 app.use("/api",routes)
 const PORT = process.env.PORT || 8000;
 
